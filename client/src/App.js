5fc3ce50f25083ee4d21018719fa5af02611f901
Sudheer Paladugu <sudheerpaladugu@yahoo.com>
2019-02-02 10:46:56 -0600

    Adding initial files

--- client/src/App.js
+++ client/src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator</h1>
+            <h1 className="App-title">Fib Calculator V2.0</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
-            <Route exact path="/" component={Fib} />
+            <Route exact path="/" component={Fib} />&nbsp;&nbsp;&nbsp;
             <Route path="/otherpage" component={OtherPage} />
           </div>
         </div>
